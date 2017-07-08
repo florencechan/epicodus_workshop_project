e5058d724f68f40222a8ae093b855e12ca493ef9
oliviauong <olivia.uong@gmail.com>
2017-07-08 13:36:31 -0700

    add workshop project to git

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// alert(2 + 2);
+// alert(4 * "2");
+// alert(8 + "2");
+$(document).ready(function() {
+  alert($("#quote").text());
+})
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
